93a15f775f940ba41c50d17a73a0de2a3a5397c5
raulalvesre <raul.alves.re@gmail.com>
2021-12-17 15:00:12 -0300

    chore: Remove unused imports

--- src/test/java/br/com/alura/school/course/CourseControllerTest.java
+++ src/test/java/br/com/alura/school/course/CourseControllerTest.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
